df2d5a366d9c637a9f251b15f24f18a4fc33ebb6
janakigorantla22 <janakigorantla22@gmail.com>
2026-06-23 15:46:04 +0530

    Create twosum.java

---
+++ twosum.java
@@ -0,0 +1,15 @@
+//without using HASHMAPS
+//APPROACH USED----- BRUTE FORCE
+  class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for(i=0;i<nums.length;i++){
+            for( j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target)
+                {
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{};
+  }
+}
